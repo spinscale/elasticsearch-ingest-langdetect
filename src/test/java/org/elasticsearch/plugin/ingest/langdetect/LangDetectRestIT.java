61c547b6104e97eac9e7fb55e6527e529557efb4
Alexander Reelsen <alexander@reelsen.net>
2017-07-07 10:27:16 +0200

    Dependencies: Upgrade to Elasticsearch 5.5.0

--- src/test/java/org/elasticsearch/plugin/ingest/langdetect/LangDetectRestIT.java
+++ src/test/java/org/elasticsearch/plugin/ingest/langdetect/LangDetectRestIT.java
@@ -22,8 +22,6 @@ import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
 
-import java.io.IOException;
-
 public class LangDetectRestIT extends ESClientYamlSuiteTestCase {
 
     public LangDetectRestIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
@@ -31,7 +29,7 @@ public class LangDetectRestIT extends ESClientYamlSuiteTestCase {
     }
 
     @ParametersFactory
-    public static Iterable<Object[]> parameters() throws IOException {
+    public static Iterable<Object[]> parameters() throws Exception {
         return ESClientYamlSuiteTestCase.createParameters();
     }
 }
